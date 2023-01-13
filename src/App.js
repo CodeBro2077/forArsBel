0f0df5cfd49dff367754b141f63e89087602b605
CodeBro2077 <118872825+CodeBro2077@users.noreply.github.com>
2023-01-13 22:51:44 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './index.scss';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={decrement} className="minus">- Минус</button>
+        <button onClick={increment} className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
